fb4e6d94fb483ffad747a594b067928fed0927a3
David Booth <david@dbooth.org>
2014-04-24 16:09:44 -0400

    Made set_env.sh more automatic

--- set_env.sh
+++ set_env.sh
@@ -1,15 +1,41 @@
 #! /bin/sh
 
 # Set environment variables needed for testing RDF::Pipeline.
-# These should be customized for your installation.
 
-# Apache DOCUMENT_ROOT for RDF-Pipeline:
-export RDF_PIPELINE_WWW_DIR=/home/dbooth/rdf-pipeline/Private/www
+# Only the RDF_PIPELINE_DEV_DIR must be set manually.
+# All others are (normally) set automatically by this script.
+#
+# RDF_PIPELINE_DEV_DIR must be set to the top development directory 
+# for the RDF::Pipeline project (as checked out from github), which must
+# contain the module directory "RDF-Pipeline".
 
-# Top development directory for the RDF::Pipeline project, which must
-# contain the module directory "RDF-Pipeline", and the module
-# directory must contain the test directory "t":
 export RDF_PIPELINE_DEV_DIR=/home/dbooth/rdf-pipeline/trunk
+
+#################################################################
+# All variables beyond this point are set automatically (unless
+# something goes wrong).
+#################################################################
+
+# Set RDF_PIPELINE_WWW_DIR.  Use $DOCUMENT_ROOT if set, or look
+# in the default Apache2 config file for the DocumentRoot definition.
+if [ ! "$DOCUMENT_ROOT" ]
+then
+	APACHECONFIG="/etc/apache2/sites-available/default"
+	DOCROOT=`grep '^[ \t]*DocumentRoot[ \t]' "$APACHECONFIG" | sed 's/^[ \t]*DocumentRoot[ \t]*//'`
+	WORDCOUNT=`echo "$DOCROOT" | wc -w`
+	if [ $WORDCOUNT = 1 ]
+	then
+		export DOCUMENT_ROOT="$DOCROOT"
+	elif [ $WORDCOUNT = 0 ]
+	then
+		echo '[ERROR] DocumentRoot not found in' "$APACHECONFIG" 1>&2
+		exit 1
+	else
+		echo '[ERROR] Multiple DocumentRoot definitions found in' "$APACHECONFIG": "$DOCROOT" 1>&2
+		exit 1
+	fi
+fi
+export RDF_PIPELINE_WWW_DIR="$DOCUMENT_ROOT"
 
 # Perl library path:
 export PERL5LIB="$PERL5LIB:$RDF_PIPELINE_DEV_DIR/RDF-Pipeline/lib"
