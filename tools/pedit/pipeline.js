9386c1a1a212e9e4ecd5b5727390b5e2069433ef
David Booth <david@dbooth.org>
2015-09-19 01:16:08 -0400

    Got pipeline.js working with custom label

--- tools/pedit/pipeline.js
+++ tools/pedit/pipeline.js
@@ -16,22 +16,33 @@ var connect = function(source, sourcePort, target, targetPort) {
     graph.addCell(link);
 };
 
-var c1 = new joint.shapes.devs.Coupled({
-    position: { x: 260, y: 150 },
-    size: { width: 300, height: 300 },
-    inPorts: ['in'],
-    outPorts: ['out 1','out 2']
-});
+var node = function(label, options) {
 
-var a1 = new joint.shapes.devs.Atomic({
+	var atomic = joint.shapes.devs.Model.extend({
+	    defaults: joint.util.deepSupplement({
+		type: 'devs.Atomic',
+		size: { width: 80, height: 80 },
+		attrs: {
+		    '.body': { fill: 'salmon' },
+		    '.label': { text: label },
+		    '.inPorts .port-body': { fill: 'PaleGreen' },
+		    '.outPorts .port-body': { fill: 'Tomato' }
+		}
+	    }, joint.shapes.devs.Model.prototype.defaults)
+	});
+
+	return new atomic(options);
+};
+
+var a1 = new node('a1', {
     position: { x: 360, y: 360 },
-    inPorts: ['port XY'],
-    outPorts: ['x','y']
+    inPorts: ['a1.in'],
+    outPorts: ['a1.out1','a1.out2']
 });
 
 var a2 = new joint.shapes.devs.Atomic({
     position: { x: 50, y: 260 },
-    outPorts: ['out']
+    outPorts: ['a2.out']
 });
 
 var a3 = new joint.shapes.devs.Atomic({
@@ -40,14 +51,9 @@ var a3 = new joint.shapes.devs.Atomic({
     inPorts: ['a','b']
 });
 
-graph.addCell(c1).addCell(a1).addCell(a2).addCell(a3);
+graph.addCell(a1).addCell(a2).addCell(a3);
 
-c1.embed(a1);
-
-connect(a2,'out',c1,'in');
-connect(c1,'in',a1,'port XY');
-connect(a1,'x',c1,'out 1');
-connect(a1,'y',c1,'out 2');
-connect(c1,'out 1',a3,'a');
-connect(c1,'out 2',a3,'b');
+connect(a2,'a2.out',a1,'a1.in');
+connect(a1,'a1.out1',a3,'a');
+connect(a1,'a1.out2',a3,'b');
 
